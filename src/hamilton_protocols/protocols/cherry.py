1fa40ccb18cbad262be05e20a3809b208c0888a1
finyl <finlay.peterkin@gmail.com>
2025-05-27 12:08:21 +0200

    feat: cherry picking

--- src/hamilton_protocols/protocols/cherry.py
+++ src/hamilton_protocols/protocols/cherry.py
@@ -1,53 +1,136 @@
-from hamilton_protocols.utils import alpha_to_index
-from adaptyv_lab import Protocol
-from rich import print
 import pandas as pd
+from adaptyv_lab import Protocol
+from pydantic import BaseModel, Field, field_validator
+from rich import print
+from hamilton_protocols.api.main import CSVData
+from hamilton_protocols.utils import alpha_to_index
+from hamilton_protocols import LAYOUTS_PATH
 
-lay_file = "C:\\Users\\Adaptyvbio\\dev\\hamilton-protocols\\src\\hamilton_protocols\\layouts\\cherry-pick.lay"
-cherry_pick_file = "C:\\Users\\Adaptyvbio\\Documents\\MAPS\\CHP-003.csv"
 
-mapping = pd.read_csv(cherry_pick_file)
-src_plate_names = mapping["Source Plate"].unique()
-print(f"Source plates: {src_plate_names}")
+class CherryPickPlateParams(BaseModel):
+    """Parameters for configuring a Cherry Pick plate."""
 
-protocol = Protocol.from_layout("test", lay_file, simulator_mode=False)
+    plate_id: str = Field(..., description="The ID of the plate.")
+    csv_data: str = Field(
+        ...,
+        description="CSV data containing the mapping of source to destination wells.",
+    )
 
-if protocol.deck is None:
-    raise ValueError("No deck found in the layout file.")
-print(protocol.deck.grid)
+    @field_validator("csv_data")
+    @classmethod
+    def validate_csv_data(cls, v):
+        if v is None:
+            return v
 
-tip_rack = protocol.deck.get_tip_rack("E1")
-src_plates = protocol.deck.get_plate_stack("F4")[: len(src_plate_names)]
-dst_plates = protocol.deck.get_plate_stack("F3")[: len(src_plate_names)][::-1]
-src_plate = protocol.deck.get_plate("C3")
-dst_plate = protocol.deck.get_plate("C2")
+        try:
+            data = CSVData(v)
+        except Exception as e:
+            raise ValueError(f"Invalid CSV data: {e!s}")
 
-tips = tip_rack.to_list()
+        required_columns = [
+            "Source Plate",
+            "Source Well",
+            "Destination Plate",
+            "Destination Well",
+        ]
+        missing = [col for col in required_columns if col not in data.df.columns]
+        if missing:
+            raise ValueError(
+                f"Well mapping CSV missing required columns: {', '.join(missing)}"
+            )
+        return v
 
-protocol.initialize()
+    @property
+    def df(self) -> pd.DataFrame:
+        """Parse well mapping CSV data into a pandas DataFrame."""
+        return CSVData(self.csv_data).df
 
-for src_plate_name in src_plate_names:
-    protocol.grip_get(src_plates.pop())
-    protocol.grip_place(src_plate, eject_tool=1)
+    @property
+    def mapping(self) -> dict[str, dict[str, str]]:
+        """Get a mapping of source wells to destination wells."""
+        plate_map = {}
+        for src_plate_id in self.df["Source Plate"].unique():
+            if src_plate_id not in plate_map:
+                plate_map[src_plate_id] = {}
+            src_wells = self.df[self.df["Source Plate"] == src_plate_id][
+                "Source Well"
+            ].to_list()
+            dst_wells = self.df[self.df["Source Plate"] == src_plate_id][
+                "Destination Well"
+            ].to_list()
+            well_map = {
+                src_well: dst_well for src_well, dst_well in zip(src_wells, dst_wells)
+            }
+            plate_map[src_plate_id] = well_map
+        return plate_map
 
-    print(f"Processing source plate: {src_plate_name}")
 
-    plate_df = mapping[mapping["Source Plate"] == src_plate_name]
-    for row in plate_df.iterrows():
-        source_well = row[1]["Source Well"]
-        dest_well = row[1]["Destination Well"]
-        print(f"Source: {source_well} in {src_plate_name} -> Dest: {dest_well}")
+class CherryPickProtocolParams(BaseModel):
+    """Parameters for configuring a Cherry Pick protocol."""
 
-        protocol.transfer(
-            src_plate[*alpha_to_index(source_well)],
-            dst_plate[*alpha_to_index(dest_well)],
-            tips.pop(0),
-            30,
+    plates: list[CherryPickPlateParams] = Field(
+        default_factory=list,
+        title="Cherry Pick Plates",
+        description="List of Cherry Pick plate parameters.",
+        min_length=1,
+        max_length=4,
+    )
+
+    @property
+    def source_plates(self) -> list[str]:
+        """Get the source plate names from the parameters."""
+        return sorted(
+            {
+                source_plate
+                for plate in self.plates
+                for source_plate in plate.df["Source Plate"].unique()
+            }
         )
 
-    protocol.grip_get(src_plate)
-    protocol.grip_place(dst_plates.pop())
 
+def cherry_pick_protocol(
+    params: CherryPickProtocolParams,
+    simulate: bool = False,
+    protocol: Protocol | None = None,
+) -> Protocol:
+    """Cherry pick protocol"""
 
-async for response in protocol.run():
-    print(response)
+    protocol = Protocol.from_layout(
+        name="Cherry Pick Protocol",
+        layout_file=LAYOUTS_PATH / "cherry-pick.lay",
+        simulator_mode=simulate,
+    )
+    if not protocol.deck:
+        msg = "Protocol deck is not defined. Please check the layout file."
+        raise ValueError(msg)
+
+    plates = params.plates
+    src_plate_ids = params.source_plates
+
+    tip_rack = protocol.deck.get_tip_rack("E1")
+    src_plates = protocol.deck.get_plate_stack("F4")[: len(src_plate_ids)]
+    dst_plates = protocol.deck.get_plate_stack("F3")[: len(src_plate_ids)][::-1]
+    src_plate = protocol.deck.get_plate("C3")
+    dst_plate = protocol.deck.get_plate("C2")
+
+    tips = tip_rack.iterlabware()
+
+    for plate in plates:
+        mapping = plate.mapping
+        for src_plate_id, src_plate_map in mapping.items():
+            src_plate_wells = [k for k in src_plate_map.keys()]
+
+            protocol.grip_get(src_plates.pop()).grip_place(src_plate)
+
+            for group in src_plate[src_plate_wells].itergroups():
+                group_src_wells = [pos.alphanumeric for pos in group]
+                group_dst_wells = [src_plate_map[pos.alphanumeric] for pos in group]
+                tips_to_use = next(tips)
+                protocol.pickup_tips(tips_to_use)
+                protocol.aspirate(src_plate[group_src_wells], 20).dispense(
+                    dst_plate[group_dst_wells], 20
+                )
+                protocol.eject_tips(tips_to_use)
+
+            protocol.grip_get(src_plate).grip_place(dst_plates.pop())
+    return protocol
